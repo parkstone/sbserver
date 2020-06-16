cc4c58c4e34a6f79206d64e9e02c53ec79eba9db
StonePark <seokhyung_park@amano.co.kr>
2020-06-16 14:50:07 +0900

    test commit

--- src/main/java/com/example/sbserver/SbserverApplication.java
+++ src/main/java/com/example/sbserver/SbserverApplication.java
@@ -7,5 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SbserverApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SbserverApplication.class, args);
+		System.out.println("test");
+
 	}
 }
