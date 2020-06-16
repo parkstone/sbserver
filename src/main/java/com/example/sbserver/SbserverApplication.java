2a1207260fb7b73061d320bb237beb60110fe939
StonePark <seokhyung_park@amano.co.kr>
2020-06-16 14:41:06 +0900

    Initial commit

---
+++ src/main/java/com/example/sbserver/SbserverApplication.java
@@ -0,0 +1,11 @@
+package com.example.sbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbserverApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SbserverApplication.class, args);
+	}
+}
