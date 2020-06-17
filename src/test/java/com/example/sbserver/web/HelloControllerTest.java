5024e208318638a8276513c444a0288311fc1db3
StonePark <seokhyung_park@amano.co.kr>
2020-06-17 18:17:12 +0900

    test commit4

--- src/test/java/com/example/sbserver/web/HelloControllerTest.java
+++ src/test/java/com/example/sbserver/web/HelloControllerTest.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org. springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
