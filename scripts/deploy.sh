ac153c1ca4b89d8cdda9941b24df5b6dab6d4c39
stonepark <seokhyung_park@amano.co.kr>
2020-12-04 13:57:29 +0900

    ~365-5page

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,8 +7,7 @@ echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-#CURRENT_PID=$(pgrep -fl sbserver | grep jar | awk '{print $1}')
-CURRENT_PID=$(pgrep -fl sbserver)
+CURRENT_PID=$(pgrep -fl sbserver | grep jar | awk '{print $1}')
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
